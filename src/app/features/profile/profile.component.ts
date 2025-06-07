dd0b1070e295f4156fe3d17a5b68e0ea01bee976
Nikola Tucek <nikola.tucek@hotmail.com>
2025-06-07 23:06:04 +0200

    Profile start

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-profile',
-    imports: [CommonModule],
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.scss']
+  selector: 'app-profile',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  id: string | null = '';
 
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('id');
+
+        if (this.id != null) {
+       /* const id = this.eventId;
+        this.eventService.getEventById(parseInt(id)).subscribe((response) => {
+          this.event = response;
+        }); */
+      }
+    });
+  }
 }
