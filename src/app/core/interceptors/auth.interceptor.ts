954d2dec4ba410879475224b509485c817ab4ffe
Nikola Tucek <nikola.tucek@hotmail.com>
2025-06-12 13:12:06 +0200

    Accept header

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -29,7 +29,6 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json; charset=utf-8',
        'Accept': 'application/json',
     });
 
