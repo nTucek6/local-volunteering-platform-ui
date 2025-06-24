6479f555a577dad095351c50cff907944817c2a3
Nikola Tucek <nikola.tucek@hotmail.com>
2025-06-24 12:44:23 +0200

    Profile getting data

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -5,24 +5,83 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { UserDto } from 'src/app/shared/dto/user.dto';
+import { EventService } from 'src/app/shared/services/event.service';
+import { EventFilterParams } from 'src/app/shared/model/event-filter-params';
+import { EventDTO } from 'src/app/shared/dto/event.dto';
+import { SearchEventDto } from 'src/app/shared/dto/search-event.dto';
 
 @Component({
   selector: 'app-profile',
-  imports: [
-    CommonModule,
-    TranslateModule,
-  ],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
   private route = inject(ActivatedRoute);
+  private userService = inject(UserService);
+  private eventService = inject(EventService);
 
   id: string | null = '';
+
+  user: UserDto | null = null;
+
+  myEvents: SearchEventDto[] = [];
+
+  myPastEvents: EventDTO[] = [];
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
+      if (this.id != null) {
+        const userId: number = Number(this.id);
+        this.userService.getUserById(userId).subscribe((response) => {
+          this.user = response;
+          console.log(this.user);
+          this.getMyEvents()
+          this.getMyPastEvents()
+        });
+      }
     });
   }
+
+  getMyEvents() {
+    const eventFilterParams: EventFilterParams = {
+      //startDateTimeFrom: new Date(new Date().setHours(0,0,0,0)),
+    };
+
+    if (this.id != null) {
+      eventFilterParams.creatorId = parseInt(this.id);
+    }
+
+    this.eventService
+      .getAllEvents(0, 5, eventFilterParams, false)
+      .subscribe((response) => {
+        this.myEvents = response;
+        console.log(this.myEvents)
+      });
+  }
+
+  getMyPastEvents() {
+    const yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday.setHours(0,0,0,0)
+
+    const eventFilterParams: EventFilterParams = {
+      startDateTimeTo: yesterday,
+    };
+
+    if (this.id != null) {
+         this.userService
+      .getUserVolunteerHistory(Number(this.id))
+      .subscribe((response) => {
+        this.myPastEvents = response;
+        console.log(this.myEvents)
+      });
+    }
+
+
+  }
+
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,16 +3,28 @@ import { environment } from 'src/environments/environment ';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserDto } from '../dto/user.dto';
+import { EventDTO } from '../dto/event.dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = `${environment.apiUrl}/auth`;
+  private apiAuthUrl = `${environment.apiUrl}/auth`;
+
+  private apiUrl = `${environment.apiUrl}/user`;
 
   private http: HttpClient = inject(HttpClient);
 
   getCurrentUser(): Observable<UserDto> {
-    return this.http.get<UserDto>(`${this.apiUrl}`);
+    return this.http.get<UserDto>(`${this.apiAuthUrl}`);
   }
+
+  getUserById(id: number): Observable<UserDto> {
+    return this.http.get<UserDto>(`${this.apiUrl}/${id}`);
+  }
+
+  getUserVolunteerHistory(id : number): Observable<EventDTO[]> {
+    return this.http.get<EventDTO[]>(`${this.apiUrl}/volunteer-history/${id}`);
+  }
+
 }
