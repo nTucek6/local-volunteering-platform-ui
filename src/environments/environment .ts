eac8bcf1868a5157668be186887bcd75ff899b7f
Nikola Tucek <nikola.tucek@hotmail.com>
2025-06-25 17:27:36 +0200

    Backend port change

--- src/environments/environment .ts
+++ src/environments/environment .ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  authUrl: 'http://localhost:8080/auth',
+  apiUrl: 'http://localhost:8081/api',
+  authUrl: 'http://localhost:8081/auth',
 };
